0827ca15e51b00c73f6b6e8f8ac8b918bc399756
Sam Bock <smbock42@gmail.com>
2025-01-07 21:12:24 -0800

    Add switch and text for RSVP notice

--- lab1-graduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ lab1-graduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -4,11 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -16,6 +31,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
 
@@ -37,20 +53,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.grad_cap),
-        contentDescription = stringResource(R.string.grad_cap_image_description),
-        alpha = 0.3f
-    )
-    Text(
-        text = stringResource(R.string.graduation_announcement),
-        fontSize = 50.sp,
-        color = Color.Red,
-        textAlign = TextAlign.Center,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 50.sp,
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier.fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = stringResource(R.string.graduation_announcement),
+            fontSize = 50.sp,
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 50.sp,
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(R.drawable.grad_cap),
+            contentDescription = stringResource(R.string.grad_cap_image_description),
+            alpha = 0.3f
+        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Filled.Info,
+                contentDescription = stringResource(R.string.icon_description),
+                tint = Color.Gray,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(40.dp)
+            )
+            Text(
+                text = stringResource(R.string.grad_location_details),
+                fontSize = 20.sp
+            )
+        }
+        RSVP(modifier)
+
+
+    }
+
 
 }
 
@@ -59,6 +99,29 @@ fun GradScreen(modifier: Modifier = Modifier) {
 fun GradPreview() {
     GraduationRSVPTheme {
         GradScreen()
+    }
+}
+
+@Preview
+@Composable
+fun RSVP(modifier: Modifier = Modifier) {
+    var isChecked by remember { mutableStateOf(true) }
+    val message = if (isChecked) "I'll be there!" else "Sorry I can't be there."
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Switch (
+            checked = isChecked,
+            onCheckedChange = {
+                isChecked = it
+            }
+        )
+        Text(
+            text = message,
+            fontSize = 30.sp
+        )
     }
 }
 
