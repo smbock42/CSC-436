997cd6b652238d9745e2f19a23255be9acb45c87
smbock42 <smbock42@gmail.com>
2025-02-07 22:09:25 -0800

    Add View models

---
+++ PetAdoption/app/src/main/java/com/zybooks/petadoption/ui/DetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.zybooks.petadoption.ui
+
+import androidx.lifecycle.ViewModel
+import com.zybooks.petadoption.data.Pet
+import com.zybooks.petadoption.data.PetDataSource
+
+class DetailViewModel : ViewModel() {
+    fun getPet(id: Int): Pet = PetDataSource().getPet(id) ?: Pet()
+}
+
