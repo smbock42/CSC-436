6f3203bdf5d7387bc1dfbb67c61c7ebd7e6c00e8
smbock42 <smbock42@gmail.com>
2025-02-18 00:24:00 -0800

    Done!

--- CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
+++ CountdownTimer/app/src/main/java/com/zybooks/countdowntimer/MainActivity.kt
@@ -62,10 +62,18 @@ class MainActivity : ComponentActivity() {
       super.onStop()
 
       // Start TimerWorker if the timer is running
-      if (timerViewModel.isRunning) {
-         startWorker(timerViewModel.remainingMillis)
+      if (timerViewModel.uiState.value.isRunning) {
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(this,
+                  Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+               startWorker(timerViewModel.uiState.value.remainingMillis)
+            }
+         } else {
+            startWorker(timerViewModel.uiState.value.remainingMillis)
+         }
       }
    }
+
 
    private fun startWorker(millisRemain: Long) {
       val timerWorkRequest: WorkRequest = OneTimeWorkRequestBuilder<TimerWorker>()
