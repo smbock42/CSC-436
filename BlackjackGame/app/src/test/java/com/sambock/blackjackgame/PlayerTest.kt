da2f533917d6926f8cedc3c23abcdc7745889171
Sam Bock <63836618+smbock42@users.noreply.github.com>
2025-02-26 16:33:13 -0800

    remove comments

--- BlackjackGame/app/src/test/java/com/sambock/blackjackgame/PlayerTest.kt
+++ BlackjackGame/app/src/test/java/com/sambock/blackjackgame/PlayerTest.kt
@@ -37,25 +37,18 @@ class PlayerTest {
         val player = Player(startingHands = 1)
         val hand = player.primaryHand()
 
-        // Set up the hand with exactly two cards of the same rank.
         val card1 = Card(Suit.HEARTS, Rank.ACE, isFaceUp = true)
         val card2 = Card(Suit.CLUBS, Rank.ACE, isFaceUp = true)
         hand.addCard(card1)
         hand.addCard(card2)
 
-        // Pre-split, there should be 1 hand.
         assertEquals("Before split, there should be 1 hand", 1, player.hands.size)
 
-        // Attempt to split the hand.
         val result = player.splitHand(0)
         assertTrue("Split should be successful", result)
-        // After splitting, we expect 2 hands.
         assertEquals("After splitting, player should have 2 hands", 2, player.hands.size)
-        // The original hand should now have 1 card.
         assertEquals("Original hand should have 1 card after split", 1, player.hands[0].getCards().size)
-        // The new hand should also have 1 card.
         assertEquals("New hand should have 1 card after split", 1, player.hands[1].getCards().size)
-        // Check that the card in the new hand is an Ace.
         val newHandCard = player.hands[1].getCards().first()
         assertEquals("Card in new hand should be an Ace", Rank.ACE, newHandCard.actualRank())
     }
@@ -65,7 +58,6 @@ class PlayerTest {
         val player = Player(startingHands = 1)
         val hand = player.primaryHand()
 
-        // Set up the hand with two cards of different ranks.
         val card1 = Card(Suit.HEARTS, Rank.ACE, isFaceUp = true)
         val card2 = Card(Suit.CLUBS, Rank.KING, isFaceUp = true)
         hand.addCard(card1)
@@ -73,7 +65,6 @@ class PlayerTest {
 
         val result = player.splitHand(0)
         assertFalse("Split should fail because cards have different ranks", result)
-        // Player still has only one hand.
         assertEquals("Player should still have one hand", 1, player.hands.size)
     }
 
@@ -82,7 +73,6 @@ class PlayerTest {
         val player = Player(startingHands = 1)
         val hand = player.primaryHand()
 
-        // Set up the hand with three cards.
         val card1 = Card(Suit.HEARTS, Rank.QUEEN, isFaceUp = true)
         val card2 = Card(Suit.CLUBS, Rank.QUEEN, isFaceUp = true)
         val card3 = Card(Suit.DIAMONDS, Rank.TWO, isFaceUp = true)
@@ -97,29 +87,22 @@ class PlayerTest {
 
     @Test
     fun testMultipleSplits() {
-        // Start with a player having two starting hands.
         val player = Player(startingHands = 2)
 
-        // Set up both hands to be eligible for splitting.
-        // For hand 0, add two Queens.
         val hand0 = player.hands[0]
         hand0.addCard(Card(Suit.HEARTS, Rank.QUEEN, isFaceUp = true))
         hand0.addCard(Card(Suit.CLUBS, Rank.QUEEN, isFaceUp = true))
-        // For hand 1, add two Tens.
+
         val hand1 = player.hands[1]
         hand1.addCard(Card(Suit.SPADES, Rank.TEN, isFaceUp = true))
         hand1.addCard(Card(Suit.DIAMONDS, Rank.TEN, isFaceUp = true))
 
-        // Attempt to split hand 0.
         val result0 = player.splitHand(0)
         assertTrue("First split should be successful", result0)
-        // Now there should be 3 hands.
         assertEquals("After first split, player should have 3 hands", 3, player.hands.size)
 
-        // Attempt to split hand 1 (which is still at index 1 after the first split).
         val result1 = player.splitHand(1)
         assertTrue("Second split should be successful", result1)
-        // Now the player should have 4 hands.
         assertEquals("After second split, player should have 4 hands", 4, player.hands.size)
     }
 }
