871966ef0f8ff51cb852ef6206cf11e8e9f1b492
Sam Bock <63836618+smbock42@users.noreply.github.com>
2025-03-18 15:40:08 -0700

    refactor: simplify Player class by removing multiple hands and enhancing primary hand functionality

--- BlackjackGame/app/src/main/java/com/sambock/blackjackgame/game/Player.kt
+++ BlackjackGame/app/src/main/java/com/sambock/blackjackgame/game/Player.kt
@@ -1,32 +1,15 @@
 package com.sambock.blackjackgame.game
 
-open class Player(startingHands: Int = 1) {
-    init {
-        require(startingHands in 1..3) { "Player must start 1 to 3 starting hands."}
+class Player {
+    private val primaryHand = Hand()
+
+    fun primaryHand(): Hand = primaryHand
+
+    fun hasBlackjack(): Boolean {
+        return primaryHand.isBlackjack()
     }
 
-    val hands: MutableList<Hand> = mutableListOf()
-
-    init {
-        repeat(startingHands) {
-            hands.add(Hand())
-        }
-    }
-
-    fun primaryHand(): Hand = hands.first()
-
-    fun splitHand(handIndex: Int): Boolean {
-        if (handIndex < 0 || handIndex >= hands.size) return false
-
-        val handToSplit = hands[handIndex]
-        val cards = handToSplit.getCards()
-
-        if (cards.size != 2 || cards[0].actualRank() != cards[1].actualRank()) return false
-
-        val removedCard = handToSplit.removeCard(cards[1]) ?: return false
-
-        val newHand = Hand().apply { addCard(removedCard) }
-        hands.add(newHand)
-        return true
+    fun getHandValue(): Int {
+        return primaryHand.getActualScore()
     }
 }
